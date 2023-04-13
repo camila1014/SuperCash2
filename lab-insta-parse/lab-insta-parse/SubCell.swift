f6976a4e2bd5234cf217e2c3f8db6c146d5e6f73
Camila Aichele <103790405+camila1014@users.noreply.github.com>
2023-04-13 10:53:02 -0400

    Add files via upload

---
+++ lab-insta-parse/lab-insta-parse/SubCell.swift
@@ -0,0 +1,22 @@
+//
+//  PostCell.swift
+//  lab-insta-parse
+//
+//  Created by Charlie Hieger on 11/3/22.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBOutlet weak var completedImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+
+    func configure(with task: Task) {
+        titleLabel.text = task.title
+        titleLabel.textColor = task.isComplete ? .secondaryLabel : .label
+        completedImageView.image = UIImage(systemName: task.isComplete ? "circle.inset.filled" : "circle")?.withRenderingMode(.alwaysTemplate)
+        completedImageView.tintColor = task.isComplete ? .systemBlue : .tertiaryLabel
+    }
+
+}
