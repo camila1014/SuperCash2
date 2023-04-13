09a3fed547818d6b5c75fd613a0860747a0c787b
Camila Aichele <103790405+camila1014@users.noreply.github.com>
2023-04-13 18:33:35 -0400

    Add files via upload

---
+++ Photo Scavenger Hunt/SuperCash/Compose/ComposeViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ComposeViewController.swift
+//  Photo Scavenger Hunt
+//
+//  Created by Camila Aichele on 2/8/23.
+//
+
+import UIKit
+
+class TaskComposeViewController: UIViewController {
+
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var priceField: UITextField!
+    
+    // When a new task is created, this closure is called, passing in the newly created task.
+    var onComposeTask: ((Task) -> Void)? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func didTapDoneButton(_ sender: Any) {
+
+        guard let title = titleField.text,
+              let description = descriptionField.text,
+              let price = priceField.text,
+              !title.isEmpty,
+              !description.isEmpty,
+              !price.isEmpty else {
+            presentEmptyFieldsAlert()
+            return
+        }
+
+        let task = Task(title: title, description: description, price: price)
+        onComposeTask?(task)
+        dismiss(animated: true)
+    }
+
+    
+    @IBAction func didTapCancelButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+
+    private func presentEmptyFieldsAlert() {
+        let alertController = UIAlertController(
+            title: "Oops...",
+            message: "Both title and description fields must be filled out",
+            preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okAction)
+
+        present(alertController, animated: true)
+    }
+}
